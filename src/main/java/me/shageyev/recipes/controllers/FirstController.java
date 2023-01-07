076fd1e5317d316f8549a9b6de353818bf2397f0
YuryShageev <schageev.yur@yandex.ru>
2023-01-07 15:25:52 +0300

    Сделал два контроллера и описание в json

--- src/main/java/me/shageyev/recipes/controllers/FirstController.java
+++ src/main/java/me/shageyev/recipes/controllers/FirstController.java
@@ -9,7 +9,7 @@ public class FirstController {
 
 @GetMapping
 public String launchInfo(){
-    return "Приложение запущено";
+    return "Приложение запущено" + ", во втором контроллере нужно поставить имя после 'info', если его нужно поставить в другом формате, то сделаю";
 }
 
 }

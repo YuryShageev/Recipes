3c8f3635605b8c06417c5489e80a6cdbc9dbd7c8
YuryShageev <schageev.yur@yandex.ru>
2023-02-01 18:37:01 +0300

    Сделал два контроллера и описание в json

--- src/main/java/me/shageyev/recipes/model/Ingredients.java
+++ src/main/java/me/shageyev/recipes/model/Ingredients.java
@@ -1,14 +1,16 @@
 package me.shageyev.recipes.model;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import org.springframework.util.StringUtils;
+
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Positive;
 
 @Data
-@AllArgsConstructor
 @EqualsAndHashCode
 public class Ingredients {
 
@@ -16,5 +18,41 @@ public class Ingredients {
     private String productName;
     @Positive
     private int amount;
+    @NotEmpty
     private String measuringUnit;
+
+    public Ingredients(String productName, int amount, String measuringUnit) {
+        this.productName = StringUtils.capitalize(productName);
+        this.amount = amount;
+        this.measuringUnit = StringUtils.trimAllWhitespace(measuringUnit);
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        if (!productName.isEmpty() || !productName.isBlank()) {
+            this.productName = productName;
+
+        } else {
+            throw new IllegalArgumentException("Название продукта обязательно");
+        }
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getMeasuringUnit() {
+        return measuringUnit;
+    }
+
+    public void setMeasuringUnit(String measuringUnit) {
+        this.measuringUnit = measuringUnit;
+    }
 }
